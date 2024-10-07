d9dab5e2897b2223f8879be6b6d310dfe9decea7
VanQish <gymorin04@gmail.com>
2024-10-07 16:07:05 +0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
-from src.Ciell import (log,countdown_timer,_clear,log_line,read_config,
+from src.ciell import (log,countdown_timer,_clear,log_line,read_config,
                        hju,pth,kng,htm,mrh,bru,reset,_banner, log_error)
